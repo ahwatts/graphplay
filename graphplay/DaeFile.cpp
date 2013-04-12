33ec0574c446c740cebaee7e7cdd9efe19c16dcf
Andrew Watts <ahwatts@gmail.com>
2013-04-12 15:12:12 -0400

    Finish extracting the vertex and normal data.

--- graphplay/DaeFile.cpp
+++ graphplay/DaeFile.cpp
@@ -109,14 +109,36 @@ FloatSource::~FloatSource() {
     }
 }
 
+void tokenizeStringToFloatArray(float *dest, char *all_floats) {
+    char *this_float = NULL, *next_float = NULL;
+    int i = 0;
+
+    this_float = strtok_r(all_floats, " ", &next_float);
+    do {
+        dest[i] = (float)atof(this_float);
+        ++i;
+        this_float = strtok_r(NULL, " ", &next_float);
+    } while (this_float != NULL);
+}
+
+void tokenizeStringToIntArray(int *dest, char *all_ints) {
+    char *this_int = NULL, *next_int = NULL;
+    int i = 0;
+
+    this_int = strtok_r(all_ints, " ", &next_int);
+    do {
+        dest[i] = (int)atoi(this_int);
+        ++i;
+        this_int = strtok_r(NULL, " ", &next_int);
+    } while (this_int != NULL);
+}
+
 FloatSource *loadSource(xmlNodePtr source_node) {
     FloatSource *rv = NULL;
-    int num_floats, i;
+    int num_floats;
     xmlNodePtr farray;
-    char *all_floats, *this_float, *next_float = NULL, *prop;
+    char *all_floats, *prop;
 
-    // We're going to ignore the accessor element and assume that the data
-    // is in blocks of 3 going like x, y, z.
     farray = source_node->children;
     while (farray != NULL) {
         if (strcmp((char*)farray->name, "float_array") == 0) {
@@ -136,13 +158,7 @@ FloatSource *loadSource(xmlNodePtr source_node) {
     rv = new FloatSource(num_floats);
 
     all_floats = (char*)xmlNodeListGetString(farray->doc, farray->children, 1);
-    this_float = strtok_r(all_floats, " ", &next_float);
-    i = 0;
-    do {
-        rv->data[i] = (float)atof(this_float);
-        ++i;
-        this_float = strtok_r(NULL, " ", &next_float);
-    } while (this_float != NULL);
+    tokenizeStringToFloatArray(rv->data, all_floats);
     xmlFree(all_floats);
 
     return rv;
@@ -174,10 +190,13 @@ std::string loadVertices(xmlNodePtr vertices_node) {
     return rv;
 }
 
-void loadPolyList(xmlNodePtr pl_node, std::map<std::string, FloatSource*> &sources) {
+FloatSource *loadPolyList(xmlNodePtr pl_node, std::map<std::string, FloatSource*> &sources) {
     xmlNodePtr curr;
-    char *vcounts, *ps, *prop;
-    int num_polys;
+    char *prop, *prop2, *vcounts_str, *ps_str;
+    int num_polys, num_verts, attrs_per_vert, vals_per_attr = 3, *vcounts, *ps;
+    std::map<int, std::string> inputs;
+    std::map<int, std::string>::const_iterator it;
+    FloatSource *rv;
 
     // How many polys are there?
     prop = (char*)xmlGetProp(pl_node, BAD_CAST "count");
@@ -187,12 +206,76 @@ void loadPolyList(xmlNodePtr pl_node, std::map<std::string, FloatSource*> &sourc
     printf("There are %d polygons.\n", num_polys);
 
     // Figure out which index is which.
-    /* curr = pl_node->children;
+    curr = pl_node->children;
     while (curr != NULL) {
         if (strcmp((char*)curr->name, "input") == 0) {
-
+            prop = (char*)xmlGetProp(curr, BAD_CAST "source");
+            prop2 = (char*)xmlGetProp(curr, BAD_CAST "offset");
+            inputs[atoi(prop2)] = std::string(prop);
+            inputs[atoi(prop2)].replace(0, 1, "");
+            xmlFree(prop);
+            xmlFree(prop2);
+        } else if (strcmp((char*)curr->name, "vcount") == 0) {
+            vcounts_str = (char*)xmlNodeListGetString(curr->doc, curr->children, 1);
+        } else if (strcmp((char*)curr->name, "p") == 0) {
+            ps_str = (char*)xmlNodeListGetString(curr->doc, curr->children, 1);
         }
-    }*/
+        curr = curr->next;
+    }
+
+    // Bail if there are problems.
+    if (!(vcounts_str && ps_str && num_polys > 0)) {
+        return NULL;
+    }
+
+    attrs_per_vert = inputs.size();
+    vals_per_attr = 3;
+
+    // Extract the vertex counts; count all the vertices.
+    vcounts = new int[num_polys];
+    tokenizeStringToIntArray(vcounts, vcounts_str);
+    num_verts = 0;
+    for (int i = 0; i < num_polys; ++i) {
+        num_verts += vcounts[i];
+    }
+
+    // Extract the source indexes.
+    ps = new int[num_verts*attrs_per_vert];
+    tokenizeStringToIntArray(ps, ps_str);
+
+    // Initialize our return value.
+    rv = new FloatSource(num_verts*attrs_per_vert*vals_per_attr);
+
+    // Copy the data from the sources into the return value.
+    for (int i = 0; i < num_verts; ++i) {
+        for (it = inputs.begin(); it != inputs.end(); ++it) {
+            const int &p_offset = it->first;
+            const std::string &source_name = it->second;
+
+            int source_index = ps[i*attrs_per_vert + p_offset];
+            int dest_index = i*attrs_per_vert*vals_per_attr + p_offset*vals_per_attr;
+            FloatSource *source = sources[source_name];
+
+            rv->data[dest_index+0] = source->data[source_index+0];
+            rv->data[dest_index+1] = source->data[source_index+1];
+            rv->data[dest_index+2] = source->data[source_index+2];
+        }
+    }
+
+    /* for (int i = 0; i < num_verts; ++i) {
+        int j = i*attrs_per_vert*vals_per_attr;
+        printf("vertex[%2d] = (%6.3f, %6.3f, %6.3f) (%6.3f, %6.3f, %6.3f)\n", i,
+               rv->data[j+0], rv->data[j+1], rv->data[j+2],
+               rv->data[j+3], rv->data[j+4], rv->data[j+5]);
+       } */
+
+    // Clean up.
+    delete [] ps;
+    delete [] vcounts;
+    xmlFree(vcounts_str);
+    xmlFree(ps_str);
+
+    return rv;
 }
 
 void loadMesh(xmlNodePtr mesh_node) {
