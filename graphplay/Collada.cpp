b914f2ce3fbbe37bf463a2327288fdc3ce10c045
Andrew Watts <ahwatts@gmail.com>
2013-07-22 21:01:15 -0400

    Still more simplification; actually parse the float_array element!

--- graphplay/Collada.cpp
+++ graphplay/Collada.cpp
@@ -3,14 +3,24 @@
 #include "Collada.h"
 #include "tinyxml2.h"
 
+#ifdef _WIN32
+#    define snprintf _snprintf_s
+#    define strdup _strdup
+#    define strtok_r strtok_s
+#endif
+
 namespace collada {
     using namespace tinyxml2;
 
-    Geometry *loadGeometry(XMLConstHandle geo_handle);
-    MeshGeometry *loadMeshGeometry(XMLConstHandle mesh_handle);
-    Source *loadSource(XMLConstHandle source_handle);
-    void loadFloatArray(std::vector<float> &array, XMLConstHandle float_array_handle);
+    // Loader methods.
+    Geometry *loadGeometry(XMLConstHandle geo_elem);
+    MeshGeometry *loadMeshGeometry(XMLConstHandle mesh_elem);
+    Source *loadSource(XMLConstHandle source_elem);
+    void loadFloatArray(std::vector<float> &array, XMLConstHandle float_array_elem);
+
+    // Utility methods.
     void handleError(const char *message);
+    void tokenizeStringToFloatArray(std::vector<float> &array, char *all_floats);
 
     // Class Geometry
     Geometry::Geometry() { }
@@ -50,95 +60,100 @@ namespace collada {
     Source::Source() : accessor(XYZAccessor(*this)), float_array() { }
     Source::~Source() { }
 
+    // Static functions.
     void loadGeometriesFromFile(std::vector<Geometry> &geos, const char* filename) {
         XMLDocument doc;
-        XMLConstHandle handle(NULL);
+        XMLConstHandle node(NULL);
         Geometry *geo;
 
         doc.LoadFile(filename);
 
-        handle = XMLConstHandle(&doc)
+        node = XMLConstHandle(&doc)
             .FirstChildElement("COLLADA")
             .FirstChildElement("library_geometries")
-            .FirstChildElement("geometry");
+            .FirstChildElement("geometry");             
 
-        if (handle.ToElement() == NULL) {
-            handleError("Could not find any geometry elements.\n");
-        }
-
-        while (handle.ToElement() != NULL) {
-            geo = loadGeometry(handle);
-            if (geo != NULL) {
-                geos.push_back(*geo);
-            }
-            handle = handle.NextSiblingElement("geometry");
+        while (node.ToElement() != NULL) {
+            geo = loadGeometry(node);
+            if (geo != NULL) { geos.push_back(*geo); }
+            node = node.NextSiblingElement("geometry");
         }
     }
 
-    Geometry *loadGeometry(XMLConstHandle geo_handle) {
-        XMLConstHandle handle(NULL);
-        Geometry *rv;
-
-        handle = geo_handle.FirstChildElement("mesh");
-
-        if (handle.ToElement() != NULL) {
-            if (strcmp(handle.ToElement()->Name(), "mesh") == 0) {
-                rv = loadMeshGeometry(handle);
-            } else {
-                handleError("Don't know how to handle a non-mesh geometry child!\n");
-            }
-        } else {
-            handleError("Geometry node has no children!\n");
-        }
-
+    Geometry *loadGeometry(XMLConstHandle geo_elem) {
+        Geometry *rv = NULL;
+        rv = loadMeshGeometry(geo_elem.FirstChildElement("mesh"));
+        // If there is no mesh element, rv will be null. We could use
+        // that to try to find other geometry types here...
         return rv;
     }
 
-    MeshGeometry *loadMeshGeometry(XMLConstHandle mesh_handle) {
-        XMLConstHandle handle(NULL);
+    MeshGeometry *loadMeshGeometry(XMLConstHandle mesh_elem) {
+        XMLConstHandle node(NULL);
         MeshGeometry *rv = new MeshGeometry();
         Source *src;
 
-        handle = mesh_handle.FirstChildElement("source");
-        if (handle.ToElement()) {
-            src = loadSource(handle);
-            if (src) { rv->sources.push_back(*src); }
-        } else {
-            handleError("Mesh node has no sources!\n");
+        // Load up the sources.
+        node = mesh_elem.FirstChildElement("source");
+        while (node.ToElement() != NULL) {
+            src = loadSource(node);
+            if (src != NULL) { rv->sources.push_back(*src); }
+            node = node.NextSiblingElement("source");
         }
 
         return rv;
     }
 
-    Source *loadSource(XMLConstHandle source_handle) {
-        XMLConstHandle handle(NULL);
+    Source *loadSource(XMLConstHandle source_elem) {
         Source *rv = new Source();
+        loadFloatArray(rv->float_array, source_elem.FirstChildElement("float_array"));
 
-        handle = source_handle.FirstChildElement("float_array");
-        if (handle.ToElement()) {
-            loadFloatArray(rv->float_array, handle);
-        } else {
-            handleError("Source node has no float_array!\n");
+        for (unsigned int i = 0; i < rv->float_array.size(); ++i) {
+            printf("%g ", rv->float_array[i]);
         }
+        printf("\n");
 
         return rv;
     }
 
-    void loadFloatArray(std::vector<float> &array, XMLConstHandle float_array_handle) {
-        const char *text;
+    void loadFloatArray(std::vector<float> &array, XMLConstHandle float_array_elem) {
+        int count = -1;
+        char *text = NULL;
+        const XMLElement *fa_node;
         const XMLText *text_node;
 
-        text_node = float_array_handle.FirstChild().ToText();
-        if (text_node) {
-            text = text_node->Value();
-            printf("Parsing float array: %s\n", text);
-        } else {
-            handleError("Float array node has no values?!\n");
+        fa_node = float_array_elem.ToElement();
+        if (fa_node != NULL) {
+            count = atoi(fa_node->Attribute("count"));
+        }
+
+        text_node = float_array_elem.FirstChild().ToText();
+        if (text_node != NULL) {
+            text = strdup(text_node->Value());
+            tokenizeStringToFloatArray(array, text);
+            free(text);
+        }
+
+        if (count != array.size()) {
+            char msg[256];
+            snprintf(msg, 256, "Expected %d floats, got %d floats!\n", count, array.size());
+            handleError((const char *)msg);
         }
     }
 
     void handleError(const char *message) {
         fprintf(stderr, message);
         exit(1);
+    }
+
+    void tokenizeStringToFloatArray(std::vector<float> &array, char *all_floats) {
+        char *this_float = NULL, *next_float = NULL;
+        int i = 0;
+
+        this_float = strtok_r(all_floats, " ", &next_float);
+        do {
+            array.push_back((float)atof(this_float));
+            this_float = strtok_r(NULL, " ", &next_float);
+        } while (this_float != NULL);
     }
 };
