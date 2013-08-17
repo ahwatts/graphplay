9e903a058414230e32db858ed8f845ef419d30e7
Andrew Watts <ahwatts@gmail.com>
2013-08-17 18:40:14 -0400

    Add project for using Google Testing to do unit testing.

---
+++ graphplay-test/GeometryTest.cpp
@@ -0,0 +1,9 @@
+// -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#include <gtest/gtest.h>
+
+#include "../graphplay/Geometry.h"
+
+TEST(Geometry, DefaultConstructor) {
+    ASSERT_TRUE(true);
+}
