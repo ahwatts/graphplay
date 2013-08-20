205250f70d01bb9ca4e209b45f14385c53fe7ddd
Andrew Watts <ahwatts@gmail.com>
2013-08-20 06:53:53 -0400

    Add stubbed-out versions of tests for Mesh and Scene.

---
+++ graphplay-test/MeshTest.cpp
@@ -0,0 +1,12 @@
+// -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#include <gtest/gtest.h>
+
+#include "../graphplay/Mesh.h"
+
+namespace graphplay {
+    TEST(MeshTest, DefaultConstructor) {
+        Mesh m;
+        ASSERT_TRUE(false);
+    }
+};
---
+++ graphplay-test/SceneTest.cpp
@@ -0,0 +1,13 @@
+// -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#include <gtest/gtest.h>
+
+#include "../graphplay/Scene.h"
+
+namespace graphplay {
+    TEST(SceneTest, DefaultConstructor) {
+        Scene s(640, 480);
+
+        ASSERT_TRUE(false);
+    }
+};
