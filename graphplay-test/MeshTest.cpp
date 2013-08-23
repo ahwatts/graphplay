c69c73f1bcbe1e074543563a8e729810d603f1d2
Andrew Watts <ahwatts@gmail.com>
2013-08-22 22:51:31 -0400

    Add some tests for Meshes.

--- graphplay-test/MeshTest.cpp
+++ graphplay-test/MeshTest.cpp
@@ -1,12 +1,39 @@
 // -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
+#include <memory>
 #include <gtest/gtest.h>
 
+#include "../graphplay/Geometry.h"
+#include "../graphplay/Material.h"
 #include "../graphplay/Mesh.h"
+#include "TestOpenGLContext.h"
 
 namespace graphplay {
-    TEST(MeshTest, DefaultConstructor) {
+    class MeshTest : public TestOpenGLContext { };
+
+    TEST_F(MeshTest, DefaultConstructor) {
         Mesh m;
-        ASSERT_TRUE(false);
+
+        wp_Geometry geo = m.getGeometry();
+        sp_Geometry sgeo = geo.lock();
+        ASSERT_EQ(nullptr, sgeo);
+
+        wp_Material mat = m.getMaterial();
+        sp_Material smat = mat.lock();
+        ASSERT_EQ(nullptr, smat);
+    }
+
+    TEST_F(MeshTest, SetGeometry) {
+        Mesh m;
+        sp_Geometry geo(new OctohedronGeometry());
+        m.setGeometry(geo);
+        ASSERT_EQ(geo, m.getGeometry().lock());
+    }
+
+    TEST_F(MeshTest, SetMaterial) {
+        Mesh m;
+        sp_Material mat(new GouraudMaterial());
+        m.setMaterial(mat);
+        ASSERT_EQ(mat, m.getMaterial().lock());
     }
 };
