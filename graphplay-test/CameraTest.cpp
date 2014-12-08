5deb3477d6a65cbdaf93b00da16c0dcd53b77ac7
Andrew Watts <ahwatts@gmail.com>
2014-12-08 00:25:35 -0500

    Fix the alignment pain in this test, too.

--- graphplay-test/CameraTest.cpp
+++ graphplay-test/CameraTest.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/string_cast.hpp>
 #include <gtest/gtest.h>
 
@@ -11,12 +12,17 @@
 namespace graphplay {
     TEST(CameraTest, DefaultConstructor) {
         Camera c;
-        ASSERT_EQ(glm::mat4x4(), c.getViewTransform());
+        glm::mat4x4 identity = glm::mat4x4(), camera_transform = c.getViewTransform();
+        const float *id_ptr = glm::value_ptr(identity), *camera_transform_ptr = glm::value_ptr(camera_transform);
+        ASSERT_EQ(id_ptr, camera_transform_ptr);
     }
 
     TEST(CameraTest, FullSetConstructor) {
         Camera c(glm::vec3(0, 0, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
         glm::mat4x4 expected = glm::translate(glm::mat4x4(), glm::vec3(0, 0, -3));
-        ASSERT_EQ(expected, c.getViewTransform());
+        glm::mat4x4 actual = c.getViewTransform();
+        const float *expected_ptr = glm::value_ptr(expected);
+        const float *actual_ptr = glm::value_ptr(actual);
+        ASSERT_EQ(expected_ptr, actual_ptr);
     }
 };
