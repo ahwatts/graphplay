a2e0a11e51253de4e1a76407f62db2a59c4fa1b2
Andrew Watts <ahwatts@gmail.com>
2014-12-17 22:50:06 -0500

    Also use the window hints when setting up the test context.

--- graphplay-test/TestOpenGLContext.cpp
+++ graphplay-test/TestOpenGLContext.cpp
@@ -14,10 +14,16 @@
 
 namespace graphplay {
     void TestOpenGLContext::SetUp() {
+        glfwSetErrorCallback(TestOpenGLContext::handle_glfw_error);
         if (!glfwInit()) {
             bailout("Could not initialize GLFW!");
         }
 
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         window = glfwCreateWindow(640, 480, "Graphplay Test Window", NULL, NULL);
 
         if (!window) {
@@ -42,5 +48,10 @@ namespace graphplay {
         std::cerr << msg << std::endl;
         glfwTerminate();
         std::exit(1);
+    }
+
+    void TestOpenGLContext::handle_glfw_error(int code, const char *desc) {
+        std::cerr << "GLFW Error Code " << code << std::endl
+                  << desc << std::endl;
     }
 };
